b615d34672a60e85b7177136da6d736a78e90d4c
admin <admin@DESKTOP-U6MBDQA>
2026-04-09 12:27:21 +0100

    Test automatic pipeline trigger

--- src/test/java/com/ecommerce/ecommerce/EcommerceApiApplicationTests.java
+++ src/test/java/com/ecommerce/ecommerce/EcommerceApiApplicationTests.java
@@ -3,6 +3,7 @@ package com.ecommerce.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//This is the test application
 @SpringBootTest
 class EcommerceApiApplicationTests {
 	@Test
